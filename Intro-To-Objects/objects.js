f2008afbc76f084f031edf1f7d9d553d2c0fe6ee
Prateek Madaan <prince.cenation@gmail.com>
2019-10-16 13:51:24 +0530

    added the code for the first video

---
+++ Intro-To-Objects/objects.js
@@ -0,0 +1,16 @@
+// null, undefined, strings, numbers, booleans and symbols are primitive values or types
+
+// Object => not primitive => arrays, functions, objects, constructors
+
+const firstName = 'Alex';
+console.log(firstName);
+
+const person = {
+  firstName: 'Prateek',
+  lastName: 'Madaan',
+  dob: '22/10/1996',
+  friends: ['Shaun','Alex','XYZ']
+}
+
+console.log(person);
+
